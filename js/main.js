90f7d4536c804d7d27e437fd0ec26f6109d72c38
Nathan C <32592701+awakedev@users.noreply.github.com>
2019-05-04 16:20:33 +0200

    output

--- js/main.js
+++ js/main.js
@@ -7,8 +7,33 @@ const searchStates = async searchText => {
     const res = await fetch('../data/states.json');
     const states = await res.json();
 
-    console.log(states);
+    // Get matches to current text input
 
+    let matches = states.filter(state => {
+        const regex = new RegExp(`^${searchText}`, 'gi');
+
+        return state.name.match(regex) || state.abbr.match(regex);
+    });
+
+    if (searchText.length === 0) {
+        matches = [];
+
+    }
+
+    outputHtml(matches);
+}
+// Show results in HTML
+
+const outputHtml = matches => {
+    if (matches.length > 0) {
+        const html = matches.map(match => `
+        <div class="card card-body mb-1">
+        <h4>${match.name} (${match.abbr}) <span class="text-primary">
+        ${match.capital}</span></h4>
+        <small>Lat: ${match.lat} / Long: ${match.long} </small>
+        </div>
+        `)
+    }
 };
 
 
